313b227fdc44efec246d39deb5c7c18eba5e48dd
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-18 00:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400)
   return ("This one is on me!") 
-} else if (someValue >= 2000)
-  return ("I will gladly take your 30 bucks!")
+} else if (someValue >= 2000) {
+} return ("I will gladly take your 30 bucks!")
 
 function ternaryCheckCity(someDistance) {
   return 
