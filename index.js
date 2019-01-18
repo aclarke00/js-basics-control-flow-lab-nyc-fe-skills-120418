7e3a61ada150b2d263d5642b0f0c9e8387c2bb63
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-18 00:40:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
   return ("This one is on me!") 
-} else if (someValue >= 2500) {
-} return ("No can do.")
-} else (someValue >= 2500)
-
-function ternaryCheckCity(someDistance) {
-  return 
-}
-}
+  } else if (someValue >= 2500) {
+   return ("No can do.") 
+  } else {
+  return ("I will glady take your thirty bucks.")
+  }
+  }
